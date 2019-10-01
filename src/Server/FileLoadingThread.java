6ea2d338f4516b57a0237d23bac40431c086bb20
Meg-geek <m.khlybova@g.nsu.ru>
2019-10-01 12:49:44 +0700

    almost complete the server thread, have some questions

--- src/Server/FileLoadingThread.java
+++ src/Server/FileLoadingThread.java
@@ -1,9 +1,8 @@
 package Server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,26 +16,38 @@ public class FileLoadingThread implements Runnable {
     private Socket clientSocket;
     private static final int DELAY_MILSEC = 3000;
     private static final int BUFSIZE = 256;
-    private long recvBytesAmount;
-    private DataInputStream in;
-    private DataOutputStream out;
+    private volatile long recvBytesAmount;
+    private final int threadNumb;
+    private long readBytes, fileSize;
+    private File file;
+   // private DataInputStream in;
+    //private DataOutputStream out;
 
-    public FileLoadingThread(Socket clientSocket){
+    public FileLoadingThread(Socket clientSocket, int threadNumb){
         this.clientSocket = clientSocket;
+        this.threadNumb = threadNumb;
     }
 
     private void startTimer(){
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
-            private int prevBytesAmount;
+            private long prevBytesAmount = 0;
+            private final double sec = DELAY_MILSEC / 1000;
+            private final long startMilSec = new Date().getTime();
             @Override
             public void run() {
-
+                long bytesAmount = recvBytesAmount;
+                double momentSpeed = (bytesAmount - prevBytesAmount) / sec;
+                prevBytesAmount = bytesAmount;
+                double allSec = ((new Date()).getTime() - startMilSec)/1000;
+                double allTimeSpeed = recvBytesAmount / allSec;
+                System.out.println("Thread " + threadNumb + ". Moment speed " + momentSpeed + " bytes per sec");
+                System.out.println("Thread " + threadNumb + ". Average speed " + allTimeSpeed + " bytes/sec");
             }
         }, DELAY_MILSEC, DELAY_MILSEC);
     }
 
-    private String readFileName() throws IOException{
+    private String readFileName(DataInputStream in, DataOutputStream out) throws IOException{
         int nameBytes = in.readInt(), readBytes = 0;
         byte[] buf = new byte[BUFSIZE];
         StringBuilder fileName = new StringBuilder();
@@ -51,23 +62,55 @@ public class FileLoadingThread implements Runnable {
         return fileName.toString();
     }
 
+    private void readFile(DataInputStream in, DataOutputStream out, String fileName) throws IOException{
+        file = new File(Server.DIR_NAME + System.lineSeparator() + fileName);
+        if(!file.createNewFile()){
+            //how?
+            System.out.println("File " + fileName + "already exists on server");
+            return; //?
+            //throw ServerException("File " + fileName + "already exists on server"); ?
+        }
+        try(FileOutputStream fileWriter = new FileOutputStream(file);){
+            fileSize = in.readLong();
+            recvBytesAmount+=8;
+            int bufSize = BUFSIZE; //bufSize ?
+            byte[] buf = new byte[BUFSIZE];
+            while(readBytes < fileSize){
+                int length = in.read(buf);
+                readBytes += length;
+                recvBytesAmount+=length;
+                fileWriter.write(buf, 0, length);
+                //нужен ли контроль буфера?
+                if(length == bufSize){
+                    buf = new byte[bufSize * 2];
+                }
+                if(length < BUFSIZE){
+                    buf = new byte[BUFSIZE];
+                }
+                //??
+            }
+        } catch(IOException ex){
+            throw ex;
+        }
+    }
+
     @Override
     public void run(){
         startTimer();
-        try {
-            in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
-            String fileName = readFileName();
-            long fileSize = in.readLong(), readBytes = 0;
-            recvBytesAmount+=8;
-            while(readBytes < fileSize){
-
-            }
-
+        try (DataInputStream in
+                     = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out
+                = new DataOutputStream(clientSocket.getOutputStream());) {
+            readFile(in, out, readFileName(in, out));
         } catch(IOException ex) {
-
+            ex.printStackTrace();
         } finally {
-
+            if(readBytes == fileSize){
+                System.out.println("Successful operation");
+            } else {
+                System.out.println("Operation wasn't successful");
+                file.delete();
+            }
         }
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,16 +5,15 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Server implements TCPServer{
-    private final String dirName = "uploads";
+    public static final String DIR_NAME = "uploads";
     private ServerSocket serverSocket;
     public static final String HOST = "localhost";
     private final int backlog = 50;
 
     public Server(int port) throws IOException {
-        File dir = new File(dirName);
+        File dir = new File(DIR_NAME);
         if(!dir.exists() && !dir.mkdir()){
             System.out.println("Can't make an upload directory");
         }
@@ -23,10 +22,12 @@ public class Server implements TCPServer{
 
     //how to handle exceptions properly?
     public void work() throws IOException{
+        int threadNumb = 0;
         while(true){
             Socket clientSocket = serverSocket.accept();
-            Thread serverThread = new Thread(new FileLoadingThread(clientSocket));
+            Thread serverThread = new Thread(new FileLoadingThread(clientSocket, threadNumb));
             serverThread.start();
+            threadNumb++;
         }
     }
 }
